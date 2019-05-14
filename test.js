9a769733717bd8a07f9b585a2c86376afd3b64de
Ricardo Romo <ricardo.romo.ramirez@gmail.com>
2019-05-14 16:35:41 -0500

    Implemented reconciliation

--- test.js
+++ test.js
@@ -1,29 +1,34 @@
 /** @jsx Didact.createElement */
 import Didact from "./lib";
+const randomLikes = () => Math.ceil(Math.random() * 100);
 const stories = [
-  { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
-  { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
-  { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
-  { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
-  { name: "Components and state", url: "http://bit.ly/2rE16nh" }
+  { name: "Didact introduction", url: "http://bit.ly/2pX7HNn", likes: randomLikes() },
+  { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH", likes: randomLikes() },
+  { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S", likes: randomLikes() },
+  { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746", likes: randomLikes() },
+  { name: "Components and state", url: "http://bit.ly/2rE16nh", likes: randomLikes() }
 ];
 
-const appElement = (
+const appElement = () => (
   <div>
     <ul>{stories.map(storyElement)}</ul>
   </div>
 );
 
-function storyElement({ name, url }) {
-  const likes = Math.ceil(Math.random() * 100);
+function storyElement(story) {
   return (
     <li>
-      <button>{likes}❤️</button>
-      <a href={url}>{name}</a>
+      <button onClick={() => handleClick(story)}>{story.likes}❤️</button>
+      <a href={story.url}>{story.name}</a>
     </li>
   );
 }
 
+function handleClick(story) {
+  story.likes += 1;
+  Didact.render(appElement(), document.getElementById("root"));
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  Didact.render(appElement, document.getElementById("root"));
+  Didact.render(appElement(), document.getElementById("root"));
 });
